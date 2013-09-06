25353a3d628ed38769bc42bd6ea75daef08cb26c
Eric Chu <eytchu@gmail.com>
2013-09-05 21:51:34 -0700

    modified ecos.py to accept None arguments for G and h. workaround for scipy's inability to accept matrices with 0 rows and 0 cols.

--- python/ecos.py
+++ python/ecos.py
@@ -7,6 +7,9 @@ def solve(c,G,h,dims,A=None,b=None):
     """ This Python routine "unpacks" scipy sparse matrices G and A into the
         data structures that we need for calling ECOS' csolve routine.
         
+        If G and h are both None, then we will automatically create an "empty"
+        CSC matrix to use with ECOS.
+        
         It is *not* compatible with CVXOPT spmatrix and matrix, although
         it would not be very difficult to make it compatible. We put the
         onus on the user to convert CVXOPT matrix types into numpy, scipy
