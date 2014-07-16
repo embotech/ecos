e03be9036354f5952a955c26b8a239a75e2590cf
Han Wang <hanwang@berkeley.edu>
2014-07-16 13:58:23 -0700

    Finished coding setup and cleanup functions

--- MISOCP/misocp.c
+++ MISOCP/misocp.c
@@ -3,113 +3,194 @@
 #include "math.h"
 #include "misocp.h"
 
+// We assume that the first num_int column vars are integers
+
 /* 
+ * ASSUMES THAT G HAS ALREADY BEEN PROCESSED BY socp_to_misocp()!!
+ *
  * Modify the input parameters before creating the template ECOS problem
  */
-misocp_pwork* miscop_setup(idxint n, idxint m, idxint p, idxint l, idxint ncones, 
-    idxint* q,
+misocp_pwork* misocp_setup(
+    idxint n, idxint m, idxint p, 
+    idxint l, idxint ncones, idxint* q,
     pfloat* Gpr, idxint* Gjc, idxint* Gir,
     pfloat* Apr, idxint* Ajc, idxint* Air,
     pfloat* c, pfloat* h, pfloat* b, idxint num_int)
 {
-    idxint i, j;
-    misocp_pwork* misocp_prob = (misocp_pwork*) malloc(sizeof(misocp_pwork));
+    idxint i;
+    
+    // Malloc the problem's memory
+    misocp_pwork* prob = (misocp_pwork*) malloc(sizeof(misocp_pwork));
 
+    // Setup the ecos solver
+    prob->ecos_prob = ECOS_setup(
+        n, m, p, l, ncones, q,
+        Gpr, Gjc, Gir,
+        Apr, Ajc, Air,
+        c, h, b);
+
+    // Malloc the initial nodes
+    prob->nodes = (misocp_node*) malloc(INITIAL_NODES*sizeof(misocp_node));
+
+    // Initialize the nodes to their default values
+    for (i=0; i<INITIAL_NODES; ++i){
+
+        prob->nodes[i].ret_code = EMPTY;
+        prob->nodes[i].last_split = 0;
+
+        // Allocate enough space in this node to hold all the constraints
+        // for identified integer variables
+        prob->nodes[i].constraints = 
+            (pfloat*) malloc(2*num_int*sizeof(pfloat));
+    }
+
+    // Store the length of the nodes array
+    prob->num_nodes = INITIAL_NODES; 
+
+    // Initialize the tmp_node
+    prob->tmp_node.constraints = (pfloat*) malloc(2*num_int*sizeof(pfloat));
+
+    // Store the number of integer variables in the problem
+    prob->num_int = num_int;
+
+    // Now initialize the root node
+    prob->nodes[i].ret_code = VALID;
+    prob->nodes[i].lb = -1e999;
+    for (i=0; i<num_int; ++i){
+        prob->nodes[i].constraints[2*i] = 1e999;
+        prob->nodes[i].constraints[2*i + 1] = -1e999;
+    }
+
+    return prob;
+}
+
+// Performs the same function as ecos_cleanup
+void misocp_cleanup(misocp_pwork* prob){
+    idxint i;
+
+    // Free solver memory
+    ecos_cleanup(prob->ecos_prob);
+
+    // Free constraints array of every node
+    for (i=0; i < prob->num_nodes; ++i){
+        free(prob->nodes[i].constraints);
+    }
+
+    // Free the nodes array
+    free(prob->nodes);
+
+    // Free the constraints array for the tmp node
+    free(prob->tmp_node.constraints);
+
+    // Free the remainder of the memory
+    free(prob);
+}
+
+
+// This function will extend the nodes array of a problem
+// by EXPAND_NODES amount
+void resize_node_array(misocp_pwork* prob){
+    idxint i;
+
+    // Resize the array
+    realloc(prob->nodes, 
+        (prob->num_nodes + EXPAND_NODES)*sizeof(misocp_node));
+
+    // Initialize the new nodes
+    for (i=num_nodes; i < (num_nodes+EXPAND_NODES); ++i){
+        
+        prob->nodes[i].ret_code = EMPTY;
+        prob->nodes[i].last_split = 0;
+
+        // Allocate enough space in this node to hold all the constraints
+        // for identified integer variables
+        prob->nodes[i].constraints = 
+            (pfloat*) malloc(2*(prob->num_int)*sizeof(pfloat));
+    }
+
+    prob->num_nodes += EXPAND_NODES;
+}
+
+
+/* Augments the G and b arrays to take lb and ub constraints
+ * for all of the variables marked integer
+ * USES MALLOC
+ */ 
+void socp_to_misocp(idxint num_int, idxint n, idxint m, idxint* l,
+    pfloat* Gpr_in, idxint* Gjc_in, idxint* Gir_in,
+    pfloat* Gpr_out, idxint* Gjc_out, idxint* Gir_out
+    pfloat* b_in, pfloat* b_out)
+{
+    idxint i;
     /** We assume that the first num_int column vars are integers **/
     
     // First we need to resize the G mat to accomodate lb and ub constraints
-    idxint* new_G_size = Gjc[n] + 2 * num_int;
-    pfloat* Gpr_new = (pfloat *) malloc( new_G_size * sizeof(pfloat) );
-    idxint* Gjr_new = (idxint *) malloc( n * sizeof(idxint) );
-    idxint* Gir_new = (idxint *) malloc( new_G_size * sizeof(idxint) );
+    idxint new_G_size = Gjc[n] + 2 * num_int;
+    Gpr_out = (pfloat *) malloc( new_G_size * sizeof(pfloat) );
+    Gjc_out = (idxint *) malloc( n * sizeof(idxint) );
+    Gir_out = (idxint *) malloc( new_G_size * sizeof(idxint) );
 
-    pfloat* b_new = (pfloat *) malloc( (n + 2 * num_int ) * sizeof(pfloat) );
+    b_out = (pfloat *) malloc( (n + 2 * num_int ) * sizeof(pfloat) );
 
     // Fill in column pointers making room for the lb's and ub's
     for (i=0; i<n ; ++i){
-        Gjc_new[i] = Gjc[i] + 2 * min(i, num_int); 
+        Gjc_out[i] = Gjc[i] + 2 * min(i, num_int); 
     }
 
     // Fill in the new matrices 
     for (i=0; i<num_int; ++i){
         if (i<num_int){
             // Set coefficients for G matrix
-            Gpr_new[ Gjc_new[i] ] = 1;
-            Gpr_new[ Gjc_new[i] + 1 ] = -1;
+            Gpr_out[ Gjc_out[i] ] = 1;
+            Gpr_out[ Gjc_out[i] + 1 ] = -1;
 
-            Gir_new[ Gjc_new[i] ] = 2*i;
-            Gir_new[ Gjc_new[i] + 1 ] = 2*i + 1;
+            Gir_out[ Gjc_out[i] ] = 2*i;
+            Gir_out[ Gjc_out[i] + 1 ] = 2*i + 1;
 
             // Copy data
-            memcpy(&Gpr[Gjr[i]] , &Gpr_new[Gjr_new[i] + 2]
-                (Gjr[i+1] - Gjr[i]) * sizeof(pfloat) );        
+            memcpy(&Gpr[Gjr[i]] , &Gpr_out[Gjr_out[i] + 2]
+                (Gjr[i+1] - Gjr[i]) *sizeof(pfloat) );        
 
             // Copy indexes
-            memcpy(&Gir[Gjr[i]] , &Gir_new[Gjr_new[i] + 2]
-                (Gjr[i+1] - Gjr[i]) * sizeof(idxint) );        
+            memcpy(&Gir[Gjr[i]] , &Gir_out[Gjr_out[i] + 2]
+                (Gjr[i+1] - Gjr[i]) *sizeof(idxint) );        
 
             // Set upper bound to +Inf
-            b_new[ 2*i ] = 1e999;     
+            b_out[ 2*i ] = 1e999;     
 
             // Set lower bound to -Inf
-            b_new[ 2*i + 1] = -1e999;     
+            b_out[ 2*i + 1] = -1e999;     
         } else {
             // Copy data
-            memcpy(&Gpr[Gjr[i]] , &Gpr_new[ Gjr_new[i] ]
-                (Gjr[i+1] - Gjr[i]) * sizeof(pfloat) );        
+            memcpy(&Gpr[Gjr[i]] , &Gpr_out[ Gjr_out[i] ]
+                (Gjr[i+1] - Gjr[i]) *sizeof(pfloat) );        
 
             // Copy indexes
-            memcpy(&Gir[Gjr[i]] , &Gir_new[ Gjr_new[i] ]
-                (Gjr[i+1] - Gjr[i]) * sizeof(idxint) );        
+            memcpy(&Gir[Gjr[i]] , &Gir_out[ Gjr_out[i] ]
+                (Gjr[i+1] - Gjr[i]) *sizeof(idxint) );        
 
         }
     }
 
-    // Increase the count for linear cones
-    l = l + num_int;
+    // Copy the remaining entries of b
+    memcpy( b_in, &b_out[2*num_int], m*sizeof(pfloat) );
 
-    // Setup the ecos solver
-    misocp_prob->ecos_prob = ECOS_setup(n, m, p, l, ncones, q,
-        Gpr_new, Gjc_new, Gir_new,
-        Apr, Ajc, Air,
-        c, h, b_new);
-
-    return misocp_prob;
+    // Augment the number of linear cones2 
+    *l = *l + num_int;
 }
 
-void modify_socp_prob( misocp_pwork* prob, misocp_node* node){
+
+/* 
+ * Updates the solver's lb and ub contraints for integer variables
+ * to the bounds specified by the node
+ */
+void modify_constraints( misocp_pwork* prob, misocp_node* node){
     memcpy( node->constraints, prob->ecos_prob->b, 
         prob->num_int *2*sizeof(pfloat));
 }
 
 int main()
 {
-    pwork *problem;
-    idxint exitflag;
-
-    /*
-    pwork* ECOS_setup(idxint n, idxint m, idxint p, idxint l, idxint ncones, idxint* q,
-    pfloat* Gpr, idxint* Gjc, idxint* Gir,
-    pfloat* Apr, idxint* Ajc, idxint* Air,
-    pfloat* c, pfloat* h, pfloat* b);
-    */
-
-    /* set up data */
-    mywork = ECOS_setup(1, 2, 0,
-        2, 0, NULL,
-        feas_Gx, feas_Gp, feas_Gi,
-        NULL, NULL, NULL,
-        feas_c, feas_h, NULL);
-
-    if( mywork != NULL ){
-        /* solve */
-        exitflag = ECOS_solve(mywork); 
-    }
-    else exitflag = ECOS_FATAL;
-        
-    /* clean up memory */
-    ECOS_cleanup(mywork, 0);
     
-    mu_assert("feas-test: ECOS failed to produce outputflag OPTIMAL", exitflag == ECOS_OPTIMAL );
     return 0;
 }
--- MISOCP/misocp.h
+++ MISOCP/misocp.h
@@ -1,11 +1,24 @@
 #ifndef __MISOCP_H__
 #define __MISOCP_H__
 
+#include "ecos.h"
+
+// MISOCP configuration settings
+#define INITIAL_NODES (100)
+#define EXPAND_NODES (100)
+
+// Preprocess flags
+#define EMPTY (-2)
+#define VALID (-1)
+
 typedef struct misocp_node {
+	// ret_code of -1 is a valid unsolved node
+	// ret_code of -2 is an overwriteable node
 	idxint ret_code;
-	idxint can_overwrite;
-	pfloat up_bound;
-	pfloat * constraints;
+	idxint last_split;
+	
+	pfloat lb;
+	pfloat* constraints;
 } misocp_node;
 
 // Wrapper for mixed integer module
@@ -13,10 +26,11 @@ typedef struct misocp_pwork{
 	// Mixed integer data
 	idxint num_int;
 	idxint num_nodes;
-	misocp_node * nodes;
+	misocp_node* nodes;
+	misocp_node tmp_node;
 
 	// ECOS data
-	pwork * ecos_prob;
+	pwork* ecos_prob;
 	
 } misocp_pwork;
 
