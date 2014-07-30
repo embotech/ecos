964470cb0bd484b5577c34098a95b88a733da6b8
Eric Chu <eytchu@gmail.com>
2014-07-29 23:53:33 -0700

    trying again

--- appveyor/install.ps1
+++ appveyor/install.ps1
@@ -87,7 +87,7 @@ function UpdateConda ($python_home) {
 function main () {
     InstallMiniconda $env:PYTHON_VERSION $env:PYTHON_ARCH $env:PYTHON
     UpdateConda $env:PYTHON
-    InstallCondaPackages $env:PYTHON "conda-build=1.4.0 pip"
+    InstallCondaPackages $env:PYTHON "conda-build=1.4.0 pip jinja2"
 }
 
 main
