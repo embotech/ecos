a712ea2b37c956cf321a5c730b69a1251aacf91e
Eric Chu <eytchu@gmail.com>
2014-07-30 01:02:37 -0700

    run builds in python directory

--- appveyor/conda-recipe/bld.bat
+++ appveyor/conda-recipe/bld.bat
@@ -1,3 +1,5 @@
 xcopy /e "%RECIPE_DIR%\..\.." "%SRC_DIR%"
+cd python
 "%PYTHON%" setup.py install bdist_wheel
+cd ..
 if errorlevel 1 exit 1
--- appveyor/conda-recipe/build.sh
+++ appveyor/conda-recipe/build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 cp -r $RECIPE_DIR/../.. $SRC_DIR
+cd python
 $PYTHON setup.py install bdist_wheel
+cd ..
