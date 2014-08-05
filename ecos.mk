4bde48ab2ef434c550ab7da25c8d1609a4a2982d
Tony Kelman <tony@kelman.net>
2014-08-05 05:37:01 -0700

    detect Windows (Cygwin or MSYS) in ecos.mk
    add dll and ecostester to CLEAN list

--- ecos.mk
+++ ecos.mk
@@ -11,16 +11,31 @@ CFLAGS = -O2 -Wall -DLDL_LONG -DDLONG -Wextra -fPIC #-ansi -ipo
 
 UNAME := $(shell uname)
 
-ifeq ($(UNAME), Linux)
-# we're on a linux system, use accurate timer provided by clock_gettime()
-LIBS = -lm -lrt
-# shared library has extension .so
-SHAREDNAME = libecos.so
+ifeq (CYGWIN, $(findstring CYGWIN, $(UNAME)))
+ISWINDOWS := 1
+else ifeq (MINGW, $(findstring MINGW, $(UNAME)))
+ISWINDOWS := 1
+else ifeq (MSYS, $(findstring MSYS, $(UNAME)))
+ISWINDOWS := 1
 else
+ISWINDOWS := 0
+endif
+
+ifeq ($(UNAME), Darwin)
 # we're on apple, no need to link rt library
 LIBS = -lm
 # shared library has extension .dylib
 SHAREDNAME = libecos.dylib
+else ifeq ($(ISWINDOWS), 1)
+# we're on windows (cygwin or msys)
+LIBS = -lm
+# shared library has extension .dll
+SHAREDNAME = libecos.dll
+else
+# we're on a linux system, use accurate timer provided by clock_gettime()
+LIBS = -lm -lrt
+# shared library has extension .so
+SHAREDNAME = libecos.so
 endif
 
 
@@ -31,4 +46,4 @@ ARCHIVE = $(AR) $(ARFLAGS)
 RANLIB = ranlib
 
 ## WHICH FILES TO CLEAN UP
-CLEAN = *.o *.obj *.ln *.bb *.bbg *.da *.tcov *.gcov gmon.out *.bak *.d *.gcda *.gcno libecos.a libecos.so libecos.dylib ecos_bb_test
+CLEAN = *.o *.obj *.ln *.bb *.bbg *.da *.tcov *.gcov gmon.out *.bak *.d *.gcda *.gcno libecos.a libecos.so libecos.dylib libecos.dll ecos_bb_test ecostester ecostester.exe
