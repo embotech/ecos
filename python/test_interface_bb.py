6d20cd5abbdf0a13109670fd716a80d85ec75607
Han Wang <hanwang@berkeley.edu>
2014-10-07 13:31:13 -0700

    Switch on equilibriate flags

--- python/test_interface_bb.py
+++ python/test_interface_bb.py
@@ -10,16 +10,19 @@ G = sp.csc_matrix( (array([2.0, 3.0, 1.0, 4.0]),
 	array([0, 1, 0, 1]), 
 	array([0, 2, 4])) )  
 
-#idxint n = 2;
-#idxint m = 2;
-#pfloat feas_Gx[4] = {2.0, 3.0, 1.0, 4.0};
-#idxint feas_Gp[3] = {0, 2, 4};
-#idxint feas_Gi[4] = {0, 1, 0, 1};
+dims = dict()
+dims['l'] = 2
 
-#pfloat feas_c[2] = {-1., -1.};
-#pfloat feas_h[2] = {4., 12.};
+sol = ecos.solve(c, G, h, dims, bool_idx=bool_idx)
 
-#idxint bool_idx[1] = {1};
+print sol['x']
+
+c = np.array([-1., -1.])
+h = np.array([ 4., 12.])
+bool_idx = np.array([0])
+G = sp.csc_matrix( (array([2.0, 3.0, 1.0, 4.0]), 
+	array([0, 1, 0, 1]), 
+	array([0, 2, 4])) )  
 
 dims = dict()
 dims['l'] = 2
