302bb983c07fa3e37f6ee6a0274589080b734b17
Alexander Domahidi <domahidi@control.ee.ethz.ch>
2014-11-14 15:41:24 +0100

    cranked down tolerances for ecos bb - want global optimal solution

--- include/ecos_bb.h
+++ include/ecos_bb.h
@@ -10,8 +10,8 @@
 
 /* ecos_bb configuration settings */
 #define MI_ABS_EPS (1E-7)
-#define MI_REL_EPS (1E-2)
-#define MI_MAXITER (100)
+#define MI_REL_EPS (1E-6)
+#define MI_MAXITER (10000)
 #define MI_INT_TOL (1E-5)
 
 /* Flags */
