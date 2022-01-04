5d3aa62ef437e41c0314b4a16427d5d06a90b7e6
Steven Diamond <diamond@cs.stanford.edu>
2022-01-04 14:33:07 -0800

    Bump version: 2.0.9 → 2.0.10

--- include/ecos.h
+++ include/ecos.h
@@ -39,7 +39,7 @@ extern "C" {
 #endif
 
 /* ECOS VERSION NUMBER - FORMAT: X.Y.Z --------------------------------- */
-#define ECOS_VERSION ("2.0.9")
+#define ECOS_VERSION ("2.0.10")
 
 /* DEFAULT SOLVER PARAMETERS AND SETTINGS STRUCT ----------------------- */
 #define MAXIT      (100)          /* maximum number of iterations         */
